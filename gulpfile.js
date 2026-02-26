426e551599885b11c3b173779119fe9f958d7423
Ivan Hawkes <ivan.hawkes@gmail.com>
2026-02-26 22:04:24 +1000

    BROKEN: cutting out unneeded things.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,52 @@
-var gulp = require('gulp');
-var path = require('path');
-var sass = require('gulp-sass')(require('sass'));
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var open = require('gulp-open');
+'use strict';
 
-var Paths = {
-    HERE: './',
-    DIST: 'dist/',
-    CSS: './static/css/',
-    SCSS_TOOLKIT_SOURCES: './static/scss/*.scss',
-    SCSS: './static/scss/**/**'
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'));
 };
 
-gulp.task('clean', () => {
-    return del([
-        'static/css/hawkes.css',
-    ]);
-});
+exports.buildStyles = buildStyles;
 
-gulp.task('compile-scss', function() {
-    return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write(Paths.HERE))
-        .pipe(gulp.dest(Paths.CSS));
-});
+// Watch the folder for changes.
+// exports.watch = function () {
+//   gulp.watch('./static/scss/*.scss', buildStyles);
+// };
+
+
+
+
+
+// var path = require('path');
+// var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps = require('gulp-sourcemaps');
+// var open = require('gulp-open');
+
+// var Paths = {
+//     HERE: './',
+//     DIST: 'dist/',
+//     CSS: './static/css/',
+//     SCSS_TOOLKIT_SOURCES: './static/scss/*.scss',
+//     SCSS: './static/scss/**/**'
+// };
+
+// gulp.task('clean', () => {
+//     return del([
+//         'static/css/hawkes.css',
+//     ]);
+// });
+
+// gulp.task('compile-scss', function() {
+//     return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+//         .pipe(sourcemaps.init())
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(autoprefixer())
+//         .pipe(sourcemaps.write(Paths.HERE))
+//         .pipe(gulp.dest(Paths.CSS));
+// });
 
 // gulp.task('watch', function() {
 //     gulp.watch(Paths.SCSS, ['compile-scss']);
@@ -40,4 +60,4 @@ gulp.task('compile-scss', function() {
 // gulp.task('open-app', ['open', 'watch']);
 
 //gulp.task('default', gulp.series(['clean', 'compile-scss']));
-gulp.task('default', gulp.series(['compile-scss']));
+//gulp.task('default', gulp.series(['compile-scss']));
